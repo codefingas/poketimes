a4a1c2c973c6e9e7623a3f47f6ac22ee068e1de4
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2020-02-10 17:00:43 +0100

    initial commit

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const contact = () => {
+
+    return (
+        <div className="container">
+            <h4 className="center">contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolore, laboriosam esse est animi quo odio nemo hic corrupti inventore rem quia aliquam illum molestias natus qui minus. Repellendus, quasi.</p>
+        </div>
+    )
+};
+
+export default contact;
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const home = () => {
+
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolore, laboriosam esse est animi quo odio nemo hic corrupti inventore rem quia aliquam illum molestias natus qui minus. Repellendus, quasi.</p>
+        </div>
+    )
+};
+
+export default home;
