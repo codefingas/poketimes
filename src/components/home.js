f9f65774acf81a313e21af56c73990ffcedcd2f4
Ezekiel Saturday <ezekielsaturday@Doro-Macbook.local>
2020-02-11 09:55:07 +0100

    fetched data using axios

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import axios from 'axios';
 
-const home = () => {
+class Home extends React.Component {
+    state = {
+        posts : []
+    };
+    
 
-    return (
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque dolore, laboriosam esse est animi quo odio nemo hic corrupti inventore rem quia aliquam illum molestias natus qui minus. Repellendus, quasi.</p>
-        </div>
-    )
-};
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    posts : res.data.slice(0, 10 )
+                })
+        })
+    };
 
-export default home;
+   render() {
+        let {posts} = this.state,
+            PostList = posts.length ? (posts.map(post => {
+                return(
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">
+                                {post.title}
+                            </span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+            ) : (<div className="center">No post yet</div>);
+
+            return (
+                <div className="container">
+                    <h4 className="center">
+                        Home
+                    </h4>
+                    {PostList}
+                </div>
+            );
+        };
+
+    }
+
+export default Home;
